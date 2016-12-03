6a3db8f906961a366355e47381a784666a520e8c
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2016-12-03 01:54:47 +0000

    added confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,12 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  config.action_mailer.delivery_method = :smtp
+
+ config.action_mailer.default_url_options = { :host => 'desolate-wave-23904.herokuapp.com', :protocol => 'https'}
+  
+  
+  
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
